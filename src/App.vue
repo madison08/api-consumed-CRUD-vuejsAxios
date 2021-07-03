80c01074459f34d91486301e1dd717d93d527b54
madison <madisonamoa@gmail.com>
2021-07-03 12:57:01 +0000

    styling with bulma and install sweetAlert

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,122 @@
 <template>
   <div class="home">
+    
+    <section class="hero is-primary">
+  <div class="hero-body">
+    <!-- <p class="title"> -->
 
+      <div class="container">
+        
+      Article
+    <!-- </p> -->
+    <p class="subtitle">
+      <!-- Primary subtitle -->
+
+      
+    </p>
     <form @submit.prevent action="">
-      <h2>Ajouter un article</h2>
+      <!-- <h2>Ajouter un article</h2> -->
+      <div class="columns">
+        <div class="column is-half">
+          <div class="field">
+            <label class="label">Titre d'article</label>
+            <div class="control is-half">
+              <input class="input" v-model="postTitle" type="text" placeholder="Ex: lorem ipsum">
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="columns">
+        <div class="column is-half">
+          <div class="field">
+            <label class="label">Titre d'article</label>
+            <div class="control">
+              <input class="input" v-model="postBody" type="text" placeholder="Ex: Dolor ammet ijh">
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <input v-model="postTitle" type="text" placeholder="titre de l'article">
-      <input v-model="postBody" type="text" placeholder="corps de l'article">
-      <button @click="createPosts" type="submit">Enregistrer</button>
+      <div class="field">
+        <p class="control">
+          <button @click="createPosts" type="submit" class="button is-success">
+            Enregistrer
+          </button>
+        </p>
+      </div>
 
     </form>
+      </div>
+  </div>
+</section>
 
+<div class="container">
 
-    <div id="parent" v-if="datas.length > 0">
-      <div v-for="data in datas" :key="data.id">
-
-        
-      <div class="simple-card">
-        <div v-if="!edit">
-          <div class="close">
-            <span @click="deletePost(data.id)">&times;</span>
+    <!-- <form @submit.prevent action="">
+      <h2>Ajouter un article</h2>
+      <div class="columns">
+        <div class="column is-half">
+          <div class="field">
+            <label class="label">Titre d'article</label>
+            <div class="control is-half">
+              <input class="input" v-model="postTitle" type="text" placeholder="Ex: lorem ipsum">
+            </div>
           </div>
+        </div>
+      </div>
+      
+      <div class="columns">
+        <div class="column is-half">
+          <div class="field">
+            <label class="label">Titre d'article</label>
+            <div class="control">
+              <input class="input" v-model="postBody" type="text" placeholder="Ex: Dolor ammet ijh">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="field">
+        <p class="control">
+          <button @click="createPosts" type="submit" class="button is-success">
+            Enregistrer
+          </button>
+        </p>
+      </div>
+
+    </form> -->
+
+
+    <div id="parent" class="columns is-multiline" style="margin-top: 40px;" v-if="datas.length > 0">
+      <div class="column is-one-third" v-for="data in datas" :key="data.id">
+
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title" @blur="updatePost(data.id)" :id="'title' + data.id" :class="{ editing: data.id === editing }" :contenteditable="data.id === editing">
+              {{ data.title }}
+
+            </p>
+            <button class="card-header-icon" aria-label="more options">
+              <span class="icon">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </header>
+          <div class="card-content">
+            <div class="content">
+            <p @blur="updatePost(data.id)" :id="'body'+data.id" :contenteditable="data.id === editing">{{ data.body.substring(0, 100) + ' ...' }}</p>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <span class="card-footer-item has-background-danger has-text-white cp" @click="deletePost(data.id)">Supprimer</span>
+            <span class="card-footer-item has-background-info has-text-white cp" @click="editPost(data.id)"> Editer </span>
+          </footer>
+        </div>
+
+
+      <!-- <div class="simple-card">
+        <div v-if="!edit">
           <div @click="editPost(data.id)" class="edit">
             <span>Editer</span>
           </div>
@@ -39,12 +136,12 @@
           </div>
         </div>
         
-      </div>
+      </div> -->
 
       </div>
     </div>
 
-    
+</div>
     
   </div>
 </template>
@@ -54,6 +151,8 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 import axios from 'axios'
+import swal from 'sweetalert';
+
 
 const apiUri = 'https://x8ki-letl-twmt.n7.xano.io/api:aGRFFROk/posts';
 
@@ -77,17 +176,26 @@ export default {
     createPosts(){
 
       axios.post(apiUri, {
-          title: this.postTitle,
-          body: this.postBody
+          titles: this.postTitle,
+          bodys: this.postBody
       })
       .then(response => {
 
         this.datas.push(response.data)
 
-        alert('utilisateur ajouter avec succes');
+        swal("Good job!", "Votre article a ete ajouter avec!", "success");
+
         // this.datas = response.data
         console.log(response)
 
+      }).catch(error => {
+
+        swal({
+          title: "Erreur lors de l'ajout"
+        });
+
+
+        console.log(error)
       })
 
     },
@@ -101,8 +209,8 @@ export default {
 
       setTimeout(() => {
 
-        document.getElementById('title'+id).style.border = '1px solid red'
-        document.getElementById('body'+id).style.border = '1px solid red'
+        document.getElementById('title'+id).style.border = '1px solid #000'
+        document.getElementById('body'+id).style.border = '1px solid #000'
 
       }, 0);
 
@@ -127,6 +235,8 @@ export default {
       })
       .then(response  => {
         console.log(response)
+        document.getElementById('title'+id).style.border = 'none'
+        document.getElementById('body'+id).style.border = 'none'
       })
 
     },
@@ -168,20 +278,20 @@ export default {
 </script>
 
 <style>
-  .simple-card{
+  /* .simple-card{
     background-color: #dbdbdb;
     border: 1px solid #000;
     margin: 5px;
     padding: 5px;
     width: 350px;
-  }
+  } */
 
-  #parent{
+  /* #parent{
     display: flex;
     align-items: center;
     flex-direction: row;
     flex-wrap: wrap;
-  }
+  } */
 
   .close{
     font-size: 25px;
@@ -200,5 +310,9 @@ export default {
     margin-left: 7px;
     cursor: pointer;
     
+  }
+
+  .cp{
+    cursor: pointer;
   }
 </style>
